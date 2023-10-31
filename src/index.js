0f52391310f3e1327a7ba968eca8f2432ac1e3f9
Samantha Callie <Hoomee90@gmail.com>
2023-10-31 16:08:13 -0700

    add beginning of functions to handle game

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import SimonGame from './simon';
 
 window.addEventListener("load", () => {
-  document.querySelectorAll("input[type='radio']").forEach((el) => {
-    el.addEventListener("click", playerButtonPress);
-  });
+  document.querySelector("button#game-start").addEventListener("click", handleGame);
 });
-
-function playerButtonPress(e) {
-  simonButtonAction(e.target)
-}
 
 function simonButtonAction(button) {
   setTimeout(() => button.checked = false, 200);
+}
+
+function handleGame() {
+  let simon = new SimonGame();
+
+  document.querySelector(".btn-group-vertical").addEventListener("click", (e) => {
+      simonButtonAction(e.target)
+  });
 }
