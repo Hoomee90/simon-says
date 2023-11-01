c2fd275a2a58b36c7731c9199c34892fa98aa363
Samantha Callie <Hoomee90@gmail.com>
2023-11-01 15:49:29 -0700

    add receive input method

--- src/simon.js
+++ src/simon.js
@@ -1,19 +1,22 @@
 export default class SimonGame {
   constructor() {
     this.sequence = [];
+    this.playerGuesses = [];
     this.position = 0;
   }
 
   addElement() {
-    this.sequence.push(Math.floor(Math.random() * 4));
+    const colors = ["red", "yellow", "green", "blue"]
+    this.sequence.push(colors[Math.floor(Math.random() * 4)]);
+  }
+  
+  receiveInput(input) {
+    this.playerGuesses.push(input);
+    return this.sequence.every((element, index) => element === this.playerGuesses[index]);
+    // return this.display()[index] === seqEl;
   }
 
-  display() {
-    const colors = ["red", "yellow", "green", "blue"];
-    return this.sequence.map(el => colors[el]);
-  }
-
-  check(seqEl, index = 0) {
-    return this.display()[index] === seqEl;
+  incrementPosition() {
+    this.position++;
   }
 }
