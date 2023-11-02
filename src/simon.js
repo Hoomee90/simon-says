07c5e678a7caa15c6e9f4a5cea0a0f64528f9de2
Samantha Callie <Hoomee90@gmail.com>
2023-11-02 14:57:18 -0700

    add method to check if round complete

--- src/simon.js
+++ src/simon.js
@@ -13,7 +13,16 @@ export default class SimonGame {
   receiveInput(input) {
     this.playerGuesses.push(input);
     return this.sequence.every((element, index) => element === this.playerGuesses[index]);
-    // return this.display()[index] === seqEl;
+  }
+
+  checkInputComplete() {
+    if (this.playerGuesses.length === this.sequence.length) {
+      this.playerGuesses = [];
+      this.position = 0;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   incrementPosition() {
